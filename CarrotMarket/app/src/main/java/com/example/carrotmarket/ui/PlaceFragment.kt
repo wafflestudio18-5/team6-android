dcac593474fca8c01b18ba86289057b0cc965d62
jwg1108 <69378301+jwg1108@users.noreply.github.com>
2021-01-07 22:22:44 +0900

    commit

--- CarrotMarket/app/src/main/java/com/example/carrotmarket/ui/PlaceFragment.kt
+++ CarrotMarket/app/src/main/java/com/example/carrotmarket/ui/PlaceFragment.kt
@@ -1,20 +1,80 @@
 package com.example.carrotmarket.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import com.example.carrotmarket.R
 import com.example.carrotmarket.databinding.FragmentPlaceBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class PlaceFragment: Fragment(){
+
+class PlaceFragment : Fragment(), OnMapReadyCallback {
+
     private lateinit var binding: FragmentPlaceBinding
+    private var mMap: GoogleMap? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_place, container, false)
 
+        val mapFragment: SupportMapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return binding.root
+    }
+
+    @SuppressLint("ServiceCast")
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap
+        /*val SEOUL: LatLng = LatLng(37.56, 126.97)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국의 수도")
+        mMap?.addMarker(markerOptions)
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 15F))*/
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            //mMap?.isMyLocationEnabled = true
+            val locationManager: LocationManager =
+                context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val locationProvider: String = LocationManager.NETWORK_PROVIDER
+            val lastKnownLocation = locationManager.getLastKnownLocation(locationProvider)
+            Log.d(
+                "tag",
+                lastKnownLocation?.latitude.toString() + " " + lastKnownLocation?.longitude.toString()
+            )
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+        }
+
+
     }
 }
